67e2ccff5b1c119aba8807e10eecbaf49d26987c
robosapien-02 <robosapien02@gmail.com>
2024-05-09 13:10:10 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,6 +1,23 @@
 class Solution:
-   
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
-                    
-        return 0
+        count = 0
+        rows = len(grid)
+        cols = len(grid[0])
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == "L": 
+                    count += 1
+                    stack = [(i, j)]
+                    while stack:
+                        row, col = stack.pop()
+                        grid[row][col] = "W"
+                        if row + 1 < rows and grid[row + 1][col] == "L":
+                            stack.append((row + 1, col))
+                        if row - 1 >= 0 and grid[row - 1][col] == "L":
+                            stack.append((row - 1, col))
+                        if col + 1 < cols and grid[row][col + 1] == "L":
+                            stack.append((row, col + 1))
+                        if col - 1 >= 0 and grid[row][col - 1] == "L":
+                            stack.append((row, col - 1))
+
+        return count
